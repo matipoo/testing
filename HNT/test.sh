64a14c978c62ecfc893a70d315c634b446a78f40
unknown <matiaspozzi@gmail.com>
2021-06-14 20:44:28 -0300

    subo script y ejemplos a probar

--- HNT/test.sh
+++ HNT/test.sh
@@ -47,6 +47,23 @@ sudo docker run \
     --publish 51.81.113.208:3000:3000/tcp \
     --name grafana \
     grafana/grafana
+
+### node ###
+
+### docker run -d -p 9100:9100 --net="host" prom/node-exporter
+
+sudo docker run \
+    -d \
+    --init \
+    --restart always \
+    --network valbridge \
+    --ip  172.28.5.6\
+    -e "NAT_INTERNAL_IP=172.28.5.6" \
+    -e "NAT_EXTERNAL_IP=51.81.113.208" \
+    --publish 51.81.113.208:9100:9100/tcp \
+    --name node \
+    prom/node-exporter
+
      
 
 
