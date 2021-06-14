db8e8495104b4fcdd1d201d781b485dbb9bdeffa
unknown <matiaspozzi@gmail.com>
2021-06-14 20:08:53 -0300

    subo script y ejemplos a probar

--- HNT/test.sh
+++ HNT/test.sh
@@ -1,10 +1,12 @@
+
+### explorer_miner ###
 sudo docker run \
     -d \
     --init \
     --restart always \
     --network valbridge \
-    --ip  172.28.5.116\
-    -e "NAT_INTERNAL_IP=172.28.5.116" \
+    --ip  172.28.5.3\
+    -e "NAT_INTERNAL_IP=172.28.5.3" \
     -e "NAT_EXTERNAL_IP=51.81.113.208" \
     --publish 51.81.113.208:9825:9825/tcp \
     --name miner_exporter \
@@ -13,4 +15,38 @@ sudo docker run \
 
 
     51.81.113.208
-    172.28.5.116
+    172.28.5.3
+
+### prometheus ###
+sudo docker run \
+    -d \
+    --init \
+    --restart always \
+    --network valbridge \
+    --ip  172.28.5.4\
+    -e "NAT_INTERNAL_IP=172.28.5.4" \
+    -e "NAT_EXTERNAL_IP=51.81.113.208" \
+    --publish 51.81.113.208:9090:9090/tcp \
+    --name prometheus \
+    -v /opt/validators/prometheus/prometheus.yml:/etc/prometheus/prometheus.yml \
+    prom/prometheus
+
+### Grafana ###
+
+## docker run -d --name=grafana -p 3000:3000 grafana/grafana
+
+
+sudo docker run \
+    -d \
+    --init \
+    --restart always \
+    --network valbridge \
+    --ip  172.28.5.5\
+    -e "NAT_INTERNAL_IP=172.28.5.5" \
+    -e "NAT_EXTERNAL_IP=51.81.113.208" \
+    --publish 51.81.113.208:3000:3000/tcp \
+    --name grafana \
+    grafana/grafana
+     
+
+
