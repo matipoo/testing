bccdef8a5e2f617fdb6d437f7629e0337f72505d
unknown <matiaspozzi@gmail.com>
2021-06-20 23:02:59 -0300

    agrego validator name mas de uno

---
+++ HNT/stop_and_remove.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+sudo docker stop bf1e15ed8474 && sudo docker rm bf1e15ed8474
+sudo docker stop 6349a1394169 && sudo docker rm 6349a1394169
+sudo docker stop 046a8423bba6 && sudo docker rm 046a8423bba6
+sudo docker stop e739c26b61b6 && sudo docker rm e739c26b61b6
+sudo docker stop f5da3509e019 && sudo docker rm f5da3509e019
+sudo docker stop f4992437026d && sudo docker rm f4992437026d
+sudo docker stop aa34a24ff93f && sudo docker rm aa34a24ff93f
+sudo docker stop 237092050a20 && sudo docker rm 237092050a20
+sudo docker stop e2f57e5f24f9 && sudo docker rm e2f57e5f24f9
+sudo docker stop d727f349b1f6 && sudo docker rm d727f349b1f6
+sudo docker stop c753e39aea01 && sudo docker rm c753e39aea01
+sudo docker stop d1f03c6013a5 && sudo docker rm d1f03c6013a5
+sudo docker stop e8ee00f5a39d && sudo docker rm e8ee00f5a39d
+sudo docker stop e01bb24f2cc8 && sudo docker rm e01bb24f2cc8
+sudo docker stop 90e46ba409e6 && sudo docker rm 90e46ba409e6
+sudo docker stop e4d0c3e15f5d && sudo docker rm e4d0c3e15f5d
+sudo docker stop b4526030f84c && sudo docker rm b4526030f84c
+sudo docker stop 23d08572cfe2 && sudo docker rm 23d08572cfe2
+sudo docker stop d090b34684c7 && sudo docker rm d090b34684c7
+sudo docker stop 4cc9787c717b && sudo docker rm 4cc9787c717b
+sudo docker stop 8bdbd6921a6f && sudo docker rm 8bdbd6921a6f
+sudo docker stop 0e8cfc1277af && sudo docker rm 0e8cfc1277af
